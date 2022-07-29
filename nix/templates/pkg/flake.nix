c08dccd97a0b054495cba53a193d13dce7f469e7
Sam Stites <stites@users.noreply.github.com>
2022-07-29 16:21:44 -0400

    add recommended nix configuration to template

--- nix/templates/pkg/flake.nix
+++ nix/templates/pkg/flake.nix
@@ -18,4 +18,12 @@
 
       defaultPackage = pkg.modRoot;
     });
+
+  # Recommended nix configuration
+  nixConfig.max-jobs = "auto";  # Allow building multiple derivations in parallel
+  nixConfig.keep-outputs = true;  # Do not garbage-collect build time-only dependencies (e.g. clang)
+
+  # Allow fetching build results from the Lean Cachix cache
+  nixConfig.extra-trusted-substituters = "https://lean4.cachix.org/";
+  nixConfig.extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= lean4.cachix.org-1:mawtxSxcaiWE24xCXXgh3qnvlTkyU7evRRnGeAhD4Wk=";
 }
